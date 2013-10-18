b70459cf7129811ef093964868eaf93042766ae1
Jakub Bouček <bukaj@snowball.bukajuv.net>
2013-10-18 22:40:21 +0200

    Init commit

---
+++ www/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="Content-language" content="cs" />
+  <meta name="google-site-verification" content="PuJ0VpFy4GhCGGBZRctXj6t-UdWm2jP2MR8W-HVA2L0" />
+  <title>MVDr. Iva Boučková</title>
+<style>
+  body {
+    background-color: #000;
+    padding: 0;
+    margin: 0; 
+    font-family: Candara, Arial, Helvetica;
+    font-size: 1.0em;
+  }
+  #page {
+    max-width: 1680px;
+    min-height: 970px;
+    height: 1571px;
+    margin: 0 auto;
+    background: #000 url(/bg.jpg) no-repeat right top;
+  }
+  
+  #contact {
+    background: url(/boxbg.png) repeat left top;
+    margin: 60px 0px 60px 120px; 
+    width: 470px;
+    padding: 20px;
+    padding-bottom: 1px;
+    color: white;
+    border-radius: 15px;
+    -webkit-border-radius: 15px;
+    -moz-border-radius: 15px;
+  }
+  
+  h1 {
+    font-size: 3em;
+    letter-spacing: 0.05em;
+    margin: 0 0 1em;
+    font-weight: normal;
+  }
+  
+  p {
+    margin-right: 1em;
+  }
+  p.sup {
+    font-size: 1.2em;
+  }
+   
+  a {
+    color: #95CB21;
+    text-decoration: none;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
+  
+  small{
+    font-style: italic;
+    color: #ccc;
+    font-size: 0.8em;
+  }
+</style>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22545774-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+  <link rel="shortcut icon" href="/favicon.ico" />  
+</head>
+
+<body>
+ <div id="page">
+ &nbsp;
+  <div id="contact">
+  <h1>MVDr. Iva Boučková</h1>
+  <p class="sup"><strong>Veterinární lékařka</strong>, <br />
+  inspektorka v&nbsp;<a href="http://www.mevs.cz/">Městské veterinární správě v&nbsp;Praze</a> a
+  vášnivá chovatelka zvěře různorodého původu</p>
+  <p>Telefon: 776 57 32 39 <br />
+  E-mail: <a href="mailto:iva&#64;bouckova.cz">iva&#64;bouckova.cz</a></p>
+  </div>
+ </div>  
+</body>
+</html>
