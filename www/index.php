43d19f2ebed354e8656e84f326ad1ef0cc06f1b1
Jakub Bouček <pan@jakubboucek.cz>
2020-12-15 01:45:24 +0100

    Update GA

--- www/index.php
+++ www/index.php
@@ -63,20 +63,23 @@
             font-size: 0.8em;
         }
     </style>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-22545774-1"></script>
     <script>
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-22545774-1']);
-        _gaq.push(['_trackPageview']);
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-        (function () {
-            var ga = document.createElement('script');
-            ga.type = 'text/javascript';
-            ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(ga, s);
-        })();
+        gtag('config', 'UA-22545774-1');
+    </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LPY9J5C974"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
+        gtag('config', 'G-LPY9J5C974');
     </script>
     <link rel="shortcut icon" href="/favicon.ico">
 </head>
